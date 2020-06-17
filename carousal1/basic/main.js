b0520e911395383b38a2f01aca38fd195459fc60
Dr. Zeb <drzeb@machomath.com>
2020-06-17 15:05:38 -0500

    Just before I gave control to Manthon

--- carousal1/basic/main.js
+++ carousal1/basic/main.js
@@ -1,7 +1,9 @@
 let leftButton = document.querySelector("button.left-button");
 let rightButton = document.querySelector("button.right-button");
+let imgDiv = document.querySelector(".img-div");
 let mainImg = document.querySelector(".img-div img");
 let numberOfImages = 5;
+var flipInterval;
 function flipImg(dir) {//dir for direction it can be 1 or -1
   let imgSrc = mainImg.src;
   let initImgSrc = imgSrc.slice(0, -5);
@@ -18,3 +20,28 @@ leftButton.addEventListener("click", function () {
 rightButton.addEventListener("click", function () {
   flipImg(1);
 });
+
+function flipImgFunction() {
+  flipInterval = setInterval(function () {
+    flipImg(1);
+  }, 3000);
+}
+
+flipImgFunction();
+
+imgDiv.addEventListener("mouseover", function () {
+  clearInterval(flipInterval);
+});
+
+imgDiv.addEventListener("mouseout", function () {
+  flipImgFunction();
+});
+
+
+
+
+
+
+
+
+//
